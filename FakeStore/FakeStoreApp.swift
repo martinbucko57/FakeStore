5e7ac7ecb267efea53bdfb8c660b6da31f109b6e
Martin Bucko <martin.bucko.91@gmail.com>
2024-11-18 12:17:18 +0100

    Initial Commit

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Martin Bucko on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
